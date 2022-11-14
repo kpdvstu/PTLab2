143135274fe89bdcf01dc41e8a6d061efb376c9b
Sav Desktop <maksim0211@gmail.com>
2022-11-14 23:15:18 +0300

    init commit

--- shop/views.py
+++ shop/views.py
@@ -2,13 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView
 
-from .models import Product, Purchase
+from .models import Product, Purchase, Discount
 
 # Create your views here.
 def index(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'shop/index.html', context)
+
+def discount_update(purchase_object):
+    discount_row = Discount.objects.get(person=purchase_object.person)
+    print(purchase_object.id)
+    price = Product.objects.get(id=purchase_object.product.id).price
+    total = discount_row.total + price
+    discount_value = total/10000.0
+    Discount.objects.filter(person=purchase_object.person).update(total=total,discount=discount_value)
 
 
 class PurchaseCreate(CreateView):
@@ -17,5 +25,15 @@ class PurchaseCreate(CreateView):
 
     def form_valid(self, form):
         self.object = form.save()
+        discount_update(self.object)
         return HttpResponse(f'Спасибо за покупку, {self.object.person}!')
+
+
+class DiscountCreate(CreateView):
+    model = Discount
+    fields = ['person', 'total', 'discount']
+
+    def form_valid(self, form):
+        self.object = form.save()
+        return HttpResponse(f'Вы зарегистрированы в программе лояльности, {self.object.person}!')
 
